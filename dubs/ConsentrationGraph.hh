6a8752e0322e0b71a69e6dbe12da703bc95d7208
banjohik <banjohik@adeb0dce-3818-11de-a9fe-ad1e22fc3a8a>
2009-05-09 19:34:15 +0000

    Added B-Spline smoothing and differentiation to ConcentrationGraph class; also created dubs subfolder to hold the source code

--- dubs/ConsentrationGraph.hh
+++ dubs/ConsentrationGraph.hh
@@ -20,12 +20,15 @@
 #include <boost/serialization/set.hpp>
 #include <boost/serialization/version.hpp>
 
+
 #include "ArtificialPancrease.hh" //contains useful typedefs and constants
 
 
 //forward declaration
 class TGraph;
 
+
+                               
 //Some defines
 enum GraphType
 {
@@ -34,6 +37,7 @@ enum GraphType
   GlucoseConsentrationGraph,
   GlucoseAbsorbtionRateGraph,
   GlucoseConsumptionGraph,
+  BloodGlucoseConcenDeriv,  //the derivative of blood-glucose concentration
   NumGraphType
 };//enum GraphType
 
@@ -49,6 +53,20 @@ enum AbsorbtionFunction
   SlowCarbAbsorbtionRate,
   NumAbsorbtionFunctions,
 };//enum AbsorbtionFunction
+
+
+//  I want to look at other spline smoothings like
+//http://www.physics.utah.edu/~detar/phys6720/handouts/cubic_spline/cubic_spline/node2.html
+                         
+enum SmoothingAlgo
+{
+  FourierSmoothing,      //Strict low-pass filter
+  ButterworthSmoothing,  //order 4 low-pass Butterworth filter
+  BSplineSmoothing,      //B-Splines, okay smoothing of data (not above link type)
+                         //  but excelentfor taking derivative
+  NoSmoothing,
+};//enum SmoothingAlgo
+
 
 class GraphElement
 {
@@ -78,6 +96,9 @@ class GraphElement
 
 BOOST_CLASS_VERSION(GraphElement, 0)
 
+
+typedef  double Filter_Coef[6][10]; //for the butterworth filter
+typedef  double Memory_Coef[3][10]; //for the butterworth filter
 
 typedef std::set<GraphElement>          GraphElementSet;
 typedef GraphElementSet::const_iterator ConstGraphIter; //set iterators const always
@@ -128,6 +149,15 @@ class ConsentrationGraph : public std::set<GraphElement>
     double value( boost::posix_time::time_duration offset ) const;
     double valueUsingOffset( double nOffsetminutes ) const;  //just a synonym for value(double)
     
+    //To obtain the derivative or smoothed version of this graph 
+    //  note that these two functions are quite computationally inefficient
+    //  (the copy this entire graph in memory a number of avoidable times)
+    ConsentrationGraph getSmoothedGraph( double wavelength, 
+                                         SmoothingAlgo smoothType) const;
+    ConsentrationGraph getDerivativeGraph( double wavelength, 
+                                           SmoothingAlgo smoothType ) const;
+    
+    
     //Add functions returns number of GraphElents added to GraphElementSet
     //  calls using posix_time::ptime, must reference a time after m_t0
     //For use with user defined function that gives blood consentration
@@ -162,18 +192,42 @@ class ConsentrationGraph : public std::set<GraphElement>
     ConstGraphIter insert( double offsetTime, double value );
     ConstGraphIter insert( const boost::posix_time::ptime &absoluteTime, double value );
    
+    double getMostCommonDt() const;
     
     
+    //You probably should not call the folowing 2 functions, 
+    //  instead call 'getSmoothedGraph(...)'
+    //For fastFourierSmoothing, lambda_min is wavelength of noise removed
+    //  e.g. bumps/noise lasting less than 0.5*lambda_min are smoothed out
+    //  tight now I do the bone head thing of just removing the frequencies
+    //  above threshold, I think I should use a convoluton???
+    void fastFourierSmooth( double lambda_min = 30.0, double time_window = -1 );
+    
+    //filterOrder gives order of the Butterworth filter (6 db/Octive, per order)
+    //  max order 4 is supported
+    void butterWorthFilter( double wavelength = 30, int filterOrder = 4 );
+
+    double bSplineSmoothOrDeriv(  bool takeDerivative,  //true turns graph onto it's 
+                                                        //own derivative
+                                                        //False just smoothes graph
+                                  double knotDist = 30,//approx distance between knots 
+                                  int splineOrder = 6 ); //4 is cubic spline
+    //below turns current graph into it's derivative, 
+    //  using finite numerical differentiation
+    void differntiate( int nPoint = 3 );  //valid nPoints are 3,5,
+
     ConsentrationGraph getTotal( const ConsentrationGraph &rhs, 
                                  const double weight = 1.0 ) const;
     ConsentrationGraph getTotal( double amountPerVol, boost::posix_time::ptime functionT0, 
                                  AbsFuncPointer ) const;
     ConsentrationGraph getTotal( double amountPerVol, boost::posix_time::ptime functionT0, 
                      AbsorbtionFunction absFunc = NumAbsorbtionFunctions) const;
-    
+
+
     void setT0_dontChangeOffsetValues( boost::posix_time::ptime newT0 );
     
     void setYOffsetForDrawing( double yOffset );
+    
     
     //Forms a TGraph and draws on current active TPad (gPad)
     TGraph* draw( std::string options = "", 
@@ -198,6 +252,17 @@ class ConsentrationGraph : public std::set<GraphElement>
     
     bool saveToFile( std::string filename );
     static ConsentrationGraph loadFromFile( std::string filename );
+    
+    //next three functions implement the Butterworth filtering, adapted it from
+    //  code found online (originally from Fortran), see cc file for reference 
+    static void butterworthInit( double Xdc, Filter_Coef C, 
+                                 int NSections, Memory_Coef D);
+    static void calcButterworthCoef( double cuttOffFrequ, double samplingTime, 
+                                     int filterOrder, Filter_Coef C,
+                                     int *NSections, double *groupDelay );
+    static void butterworthFilter( double *Xs,double Xd, int NSections,
+ 	                                          Filter_Coef C, Memory_Coef D );
+    
     
     //Serialize function has to be in the header since it's a template
     friend class boost::serialization::access;
